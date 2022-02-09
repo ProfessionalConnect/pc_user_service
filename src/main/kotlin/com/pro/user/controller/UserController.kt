205ce6a6167264a263d37246a4df9afbaafefb92
koreaaerospaceU <zx811@naver.com>
2022-02-10 00:46:32 +0900

    feat: User Controller 로직 구현

---
+++ src/main/kotlin/com/pro/user/controller/UserController.kt
@@ -0,0 +1,51 @@
+package com.pro.user.controller
+
+import com.pro.user.domain.UserRole
+import com.pro.user.dto.AuthenticationRequest
+import com.pro.user.dto.RefreshRequest
+import com.pro.user.dto.TokenResponse
+import com.pro.user.dto.UserRequest
+import com.pro.user.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+
+/**
+ * Created by Minky on 2022-02-08
+ */
+
+@RestController
+@RequestMapping("/api/v1/users")
+class UserController {
+    @Autowired
+    private lateinit var userService: UserService
+
+    @PostMapping("/members/signup")
+    fun memberSignUp(
+        @RequestBody userRequest: UserRequest
+    ): ResponseEntity<Void> {
+        val userId = userService.singUp(userRequest, UserRole.MEMBER)
+        return ResponseEntity.created(URI("/api/v1/users/${userId}")).build()
+    }
+
+    @PostMapping("/pros/signup")
+    fun proSignUp(
+        @RequestBody userRequest: UserRequest
+    ): ResponseEntity<Void> {
+        val userId = userService.singUp(userRequest, UserRole.PRO)
+        return ResponseEntity.created(URI("/api/v1/users/${userId}")).build()
+    }
+
+    @PostMapping("/signin")
+    fun signIn(
+        @RequestBody authenticationRequest: AuthenticationRequest
+    ): ResponseEntity<TokenResponse> =
+        ResponseEntity.ok().body(userService.singIn(authenticationRequest))
+
+    @PostMapping("/refresh")
+    fun refresh(
+        @RequestBody refreshRequest: RefreshRequest
+    ): ResponseEntity<TokenResponse> =
+        ResponseEntity.ok().body(userService.refresh(refreshRequest))
+}
