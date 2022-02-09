7cc414ab9983306de807c00db2ba542b3f94bd59
koreaaerospaceU <zx811@naver.com>
2022-02-10 00:46:01 +0900

    feat: User Repository 선언

---
+++ src/main/kotlin/com/pro/user/repository/UserRepository.kt
@@ -0,0 +1,16 @@
+package com.pro.user.repository
+
+import com.pro.user.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+/**
+ * Created by Minky on 2022-02-08
+ */
+
+@Repository
+interface UserRepository: JpaRepository<User, Long>{
+    fun findByEmail(email: String): Optional<User>
+    fun findByUuid(uuid: String): Optional<User>
+}
